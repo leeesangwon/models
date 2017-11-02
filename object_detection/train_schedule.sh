bdfe2f5a6a689b7aa7abb195983fdc1d24598ce5
Sangwon Lee <leee.sangwon@gmail.com>
2017-11-02 19:48:18 +0900

    modify evaluation codes, automaticaly evaluate from first ckpt to latest ckpt

--- object_detection/train_schedule.sh
+++ object_detection/train_schedule.sh
@@ -1,7 +1,12 @@
-python ./train.py \
-    --logtostderr \
-    --train_dir=`pwd`/train_rfcn_medical/4/A/ \
-    --pipeline_config_path=`pwd`/rfcn_resnet101_medical_A.config
-python ./train.py --logtostderr \
-    --train_dir=`pwd`/train_rfcn_medical/4/C/ \
-    --pipeline_config_path=`pwd`/rfcn_resnet101_medical_C.config
+# train_schedule.sh
+OBJ_DTC_PATH='/path/to/tensorflow/models/object_detection'
+# hori-flip + random-crop
+EXP_NAME='7'
+DATA_LIST=(A C B D)
+for ((i=0; i<${#DATA_LIST[*]}; i++))
+do
+    python ./train.py \
+        --logtostderr \
+        --train_dir=${OBJ_DTC_PATH}/train_rfcn_medical/${EXP_NAME}/${DATA_LIST[$i]}/ \
+        --pipeline_config_path=${OBJ_DTC_PATH}/train_rfcn_medical/${EXP_NAME}/rfcn_resnet101_medical_${DATA_LIST[$i]}.config
+done
