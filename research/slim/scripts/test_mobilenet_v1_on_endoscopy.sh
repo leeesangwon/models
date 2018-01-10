960cd068cfd8a1147ded0a490697558d2f8f05dc
Sangwon Lee <leee.sangwon@gmail.com>
2018-01-10 23:26:43 +0900

    add some shell scripts

---
+++ research/slim/scripts/test_mobilenet_v1_on_endoscopy.sh
@@ -0,0 +1,62 @@
+# This script performs the following operations:
+# 1. Downloads the endoscopy dataset
+# 2. Fine-tunes an inception resnet v2 model on the Endoscopy training set.
+# 3. Evaluates the model on the Endoscopy validation set.
+#
+# Usage:
+# cd research/slim
+# bash ./scripts/finetune_inception_resnet_v2_on_endoscopy.sh
+
+export PROJECTS_DIR=/home/sangwon/Projects/Medical
+export START_DATE=$(date '+%F')
+
+# About base model
+export PRETRAINED_CHECKPOINT_DIR=${PROJECTS_DIR}/PretrainedModel
+
+# inception resnet v2
+export MODEL_NAME='mobilenet_v1'
+# export PREPROCESSING_NAME='inception_resnet_v2_notcrop'
+export EXCLUDE_SCOPES='MobilenetV1/Logits'
+# export TRAINABLE_SCOPES='InceptionResnetV2/Logits,InceptionResnetV2/AuxLogits'
+
+# About dataset
+export DATA_SUBNAMES=(A)
+export DATA_CROSS_VAL=(1 2 3 4)
+export FOLDER_NAME=2018-01-10_crop_bbox
+
+# About training
+export TRAIN_BATCH_SIZE=32
+export EVAL_BATCH_SIZE=2
+export MAX_NUMBER_OF_STEPS=30000
+export EVALUATE_INTERVAL=500
+export START_STEP=15000
+
+for ((i=0; i<${#DATA_SUBNAMES[*]}; i++))
+do
+    for ((k=0; k<${#DATA_CROSS_VAL[*]}; k++))
+    do
+        # DATASET_DIR=${PROJECTS_DIR}/DATA/CLASSIFICATION/crop_bbox/test/data_${DATA_SUBNAMES[$i]}
+        DATASET_DIR=${PROJECTS_DIR}/DATA/CLASSIFICATION/crop_bbox/trainval/data_${DATA_SUBNAMES[$i]}
+
+        DATASET_NAME=endoscopy_${DATA_SUBNAMES[$i]}
+        # DATASET_FILE_PATTERN=cls_data_${DATA_SUBNAMES[$i]}_0_%s_*.tfrecord
+        DATASET_FILE_PATTERN=cls_data_${DATA_SUBNAMES[$i]}_${DATA_CROSS_VAL[${k}]}_%s_*.tfrecord
+
+        # About training
+        TRAIN_DIR=${PROJECTS_DIR}/TRAIN/CLASSIFICATION/${DATASET_NAME}-models/${MODEL_NAME}_${PREPROCESSING_NAME}/${FOLDER_NAME}/${DATA_CROSS_VAL[${k}]}
+        EVAL_DIR=${TRAIN_DIR}/eval
+
+        # Run evaluation.
+        python eval_image_classifier_from_0_to_end.py \
+            --batch_size=${EVAL_BATCH_SIZE} \
+            --checkpoint_path=${TRAIN_DIR} \
+            --eval_dir=${EVAL_DIR} \
+            --dataset_name=${DATASET_NAME} \
+            --dataset_split_name=validation \
+            --dataset_dir=${DATASET_DIR} \
+            --dataset_file_pattern=${DATASET_FILE_PATTERN} \
+            --model_name=${MODEL_NAME} \
+            --preprocessing_name=${PREPROCESSING_NAME}
+
+    done
+done
